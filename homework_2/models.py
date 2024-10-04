bf8cf433e1b394287b44bb392e2e70eafb218f3c
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-10-04 19:48:27 +0300

    Add files via upload

---
+++ homework_2/models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Item(BaseModel):
+    id: int
+    name: str
+    price: float
+    deleted: bool = False
+
+class Cart_Item(BaseModel):
+    id: int
+    name: str
+    quantity: int
+    available: bool
+
+class Cart(BaseModel):
+    id: int
+    items: List[Cart_Item]
+    price: float
+
+class Update_Item(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    deleted: bool = False
+
+class Create_Item(BaseModel):
+    name: str
+    price: float = Field(ge=0)  

ac82e5a8c21d628df91af2f81d222b3bc44ab5ca
ekaterina.baru <ekaterina.baru@velotix.ai>
2024-10-05 11:41:33 +0300

    check out

--- homework_2/functions.py
+++ homework_2/functions.py
@@ -4,18 +4,21 @@ from models import Item
 item_id_counter = 0
 cart_id_counter = 0
 
+
 def id_gen_item():
     global item_id_counter
     item_id_counter += 1
     yield item_id_counter
+
 
 def id_gen_cart():
     global cart_id_counter
     cart_id_counter += 1
     yield cart_id_counter
 
-def get_item_body(item_id: int) -> Item:
+
+def get_item_body(item_id: int) -> Item | None:
     item = items.get(item_id)
-    if item and not item["deleted"]:
+    if item and not item.deleted:
         return item
     return None
--- homework_2/models.py
+++ homework_2/models.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+
 class Item(BaseModel):
     id: int
     name: str
     price: float
     deleted: bool = False
+
 
 class Cart_Item(BaseModel):
     id: int
@@ -13,16 +15,22 @@ class Cart_Item(BaseModel):
     quantity: int
     available: bool
 
+
 class Cart(BaseModel):
     id: int
     items: List[Cart_Item]
     price: float
 
+
 class Update_Item(BaseModel):
     name: Optional[str] = None
     price: Optional[float] = None
-    deleted: bool = False
+    deleted: Optional[bool] = False
+
+    class Config:
+        extra = "forbid"
+
 
 class Create_Item(BaseModel):
     name: str
-    price: float = Field(ge=0)  
+    price: float = Field(ge=0)
